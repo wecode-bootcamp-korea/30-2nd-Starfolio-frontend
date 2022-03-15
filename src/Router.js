9c073cd6e9cc6a5c2387998a4b1a1d3247f3510e
Bueong <pcj0308@gmail.com>
2022-03-15 23:14:38 +0900

    Add: Footer 구조제작 및 스타일링

--- src/Router.js
+++ src/Router.js
@@ -5,7 +5,7 @@ import Login from './pages/Login/Login';
 import ItemsList from './pages/ItemsList/ItemsList';
 import ItemsDetail from './pages/ItemsDetail/ItemsDetail';
 // import Header from "../src/components/Header/Header";
-// import Footer from "../src/components/Footer/Footer";
+import Footer from '../src/components/Footer/Footer';
 
 function Router() {
   return (
@@ -17,7 +17,7 @@ function Router() {
         <Route path="/itemslist" element={<ItemsList />} />;
         <Route path="/itemsdetail" element={<ItemsDetail />} />;
       </Routes>
-      {/* <Footer/> */}
+      <Footer />
     </BrowserRouter>
   );
 }
